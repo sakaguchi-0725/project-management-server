1e32e4d033d38021635712204934d093fc833751
sakaguchi <kzmsan63@gmail.com>
2024-06-06 21:55:45 +0900

    テスト修正

--- internal/test/util/db.go
+++ internal/test/util/db.go
@@ -3,25 +3,19 @@ package testutil
 import (
 	"fmt"
 	"log"
-	"os"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func NewTestDB() *gorm.DB {
-	err := godotenv.Load("../../../.env")
-	if err != nil {
-		log.Fatalf("環境変数の読み込みに失敗しました: %v", err)
-	}
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		os.Getenv("TEST_DB_HOST"),
-		os.Getenv("TEST_DB_USER"),
-		os.Getenv("TEST_DB_PASSWORD"),
-		os.Getenv("TEST_DB_NAME"),
-		os.Getenv("TEST_DB_PORT"),
+		"localhost",
+		"postgres",
+		"postgres",
+		"todo",
+		"54321",
 	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
